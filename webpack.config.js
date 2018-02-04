5cde24125825ebe9ac186c9cde0d0258ef9b0009
Yanely <yanely_rmz@yahoo.com>
2018-02-03 19:22:43 -0500

    added webpack configs and setup locally

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
     JS: path.resolve(__dirname, 'src/js'),
 };
 
@@ -11,5 +13,10 @@ module.exports = {
         path: paths.DIST,
         filename: 'app.bundle.js',
     },
+	plugins: [
+		new htmlWebpackPlugin({
+			template: path.join(paths.SRC, 'index.html'),
+		})
+	],
 };
 
