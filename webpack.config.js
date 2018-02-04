55994ad8a779a3ab568505df7add4114f0637f72
Yanely <yanely_rmz@yahoo.com>
2018-02-03 19:36:15 -0500

    adding babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const paths = {
-    DIST: path.resolve(__dirname, 'dist'),
-    SRC: path.resolve(__dirname, 'src'),
-    JS: path.resolve(__dirname, 'src/js'),
+	DIST: path.resolve(__dirname, 'dist'),
+	SRC: path.resolve(__dirname, 'src'),
+	JS: path.resolve(__dirname, 'src/js'),
 };
 
 module.exports = {
-    entry: path.join(paths.JS, 'app.js'),
-    output: {
-        path: paths.DIST,
-        filename: 'app.bundle.js',
-    },
+	entry: path.join(paths.JS, 'app.js'),
+	output: {
+		path: paths.DIST,
+		filename: 'app.bundle.js',
+	},
 	plugins: [
-		new htmlWebpackPlugin({
+		new HtmlWebpackPlugin({
 			template: path.join(paths.SRC, 'index.html'),
-		})
+		}),
+		new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file -> ADDED IN THIS STEP
 	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+				'babel-loader',
+				],
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract({
+					use: 'css-loader',
+				}),
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
 };
-
